45c5f564b62c6cdd18f880cc6128807209d5af3b
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-29 01:00:49 +0000

    Added more jquery animation

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -15,4 +15,20 @@ $(document).ready(function() {
    };
  
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
+    
+    var onHoverAction = function(event) {
+      console.log('Hover action triggered.');
+      $(this).css({'color': '#7CFC00'});
+    };
+
+    var offHoverAction = function(event) {
+      console.log('Off-hover action triggered.');
+      $(this).css({'color': 'white'});
+    };
+
+    $('.hero-content h3').hover(onHoverAction, offHoverAction);
+
+    $('.hero-content h1').click(function(){
+      $(this).fadeOut('slow');
+    });
   });
